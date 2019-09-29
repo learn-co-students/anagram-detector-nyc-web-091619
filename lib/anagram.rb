5a167af2b1726a94d2ff26becf593bb52f589b24
github username <github email address>
2019-09-29 16:28:01 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+
+    attr_reader :name
+
+
+    # initializes with a name (word) that needs to be matched 
+
+    def initialize(name)
+        @name = name
+    end
+
+    # match method receives an array and iterates through comparing the sorted, 
+    # individual letters of each word, to "name" (also sorted and split)
+    
+    def match(array)
+        array.select { |word|
+            word.split("").sort == name.split("").sort
+        }
+    end
+
+
+end
